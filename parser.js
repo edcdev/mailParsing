559ac04b7e8daf0924da74027810bdc0f0b17a38
Pierre Riché <pierre1.riche@epitech.eu>
2017-03-22 14:31:51 +0000

    Formatting headers, half done

--- parser.js
+++ parser.js
@@ -1,6 +1,21 @@
 $(document).ready(function () {
+    var json = {};
+    var mail = $('#mail');
+    var content;
+    var pos;
 
-    $('#mail').load("test.html");
+    mail.load("test.html", function () {
+        content = mail.html();
+        content = content.replace(/\\n+/g, '');
+        content = content.replace(/\\r+/g, '');
+        pos = content.substr(content.indexOf('<meta'), content.indexOf('>'));
+        content = content.replace(pos, '');
+        if (mail.html.length){
+            json.status = 'OK';
+        }
+        console.log(content);
+    });
 
+    //console.log(json);
 
 });
