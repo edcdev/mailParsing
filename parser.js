3b2cc75e0dd32f2903c1bf6af671d854461c583a
Pierre Riché <pierre1.riche@epitech.eu>
2017-03-23 17:26:29 +0000

    almost working, missing passenger list

--- parser.js
+++ parser.js
@@ -3,6 +3,10 @@ $(document).ready(function () {
     var mail = $('#mail');
     var content;
     var pos;
+    var days = ['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'];
+    var price = '';
+    var name = '';
+    var date = [];
 
     mail.load("test.html", function () {
         content = mail.html();
@@ -17,7 +21,7 @@ $(document).ready(function () {
             title = content.match('Monsieur');
         }
         var i = title.index + title[0].length + 1;
-        var name = '';
+
 
         while (content[i] !== ' '){
             name += content[i];
@@ -29,31 +33,105 @@ $(document).ready(function () {
         transac = content.indexOf('<td', transac.index);
         transac = content.indexOf('>', transac);
         i = transac + 2;
-        var price = '';
         while (content[i] !== ' '){
             price += content[i];
             i++;
         }
-        
+        var day;
+        i = 0;
+        var pos;
+        while (i <= 6){
+            day = content.match(days[i]);
+            if (day){
+                date.push({day: day[0], pos: day.index});
+                pos = day.index;
+                while (pos != -1){
+                    pos = content.indexOf(days[i], pos + days[i].length);
+                    if (pos >= 0){
+                        date.push({day: days[i], pos: pos});
+                    }
+                }
+            }
+            i++;
+        }
+        var trips = [];
+        for (var c = 0; c < date.length; c++){
+            i = content.indexOf(date[c].day, date[c].pos);
+            var fulldate = '';
+            while (content[i] !== '<'){
+                fulldate += content[i];
+                i++;
+            }
+            i = content.indexOf('<td', date[c].pos);
+            i = content.indexOf('>', i) + 1;
+            var type = '';
+            while (content[i] !== '<'){
+                type += content[i];
+                i++;
+            }
+            i = content.indexOf('<td', i);
+            i = content.indexOf('>', i) + 1;
+            var start = '';
+            while (content[i] !== '<'){
+                start += content[i];
+                i++;
+            }
+            i = content.indexOf('<td', i);
+            i = content.indexOf('>', i) + 1;
+            var place_start = '';
+            while (content[i] !== '<'){
+                place_start += content[i];
+                i++;
+            }
+            i = content.indexOf('<td', i);
+            i = content.indexOf('>', i) + 1;
+            var train = '';
+            while (content[i] !== '<'){
+                train += content[i];
+                i++;
+            }
+            i = content.indexOf('<td', i);
+            i = content.indexOf('>', i) + 1;
+            var num = '';
+            while (content[i] !== '<'){
+                num += content[i];
+                i++;
+            }
+            while (content.indexOf('Passager', i) !== -1){
+                i = content.indexOf('<br>', i);
+                i = content.indexOf('(', i);
+                var passenger = '';
+                while (content[i] !== '<'){
+                    passenger += content[i];
+                    i++;
+                }
+                i = content.indexOf('train.', i) + 6;
+                var billet_type = '';
+                while (i !== content.indexOf('ble', i)){
+                    billet_type += content[i];
+                    i++;
+                }
+            }
+            trips.push({
+                date: fulldate,
+                type: type,
+                trains: {
+                    departureTime: start,
+                    departureStation: place_start,
+                    type: train,
+                    number: num,
+                    passenger: {
+                        age: passenger,
+                        type: billet_type
+                    }
+                }
+            });
+        }
+        console.log(trips);
         //status
         if (mail.html.length){
             json.status = 'OK';
         }
         console.log(content);
     });
-
-    /*function remove(index, array, close){
-        var pos = array.indexOf(index);
-        var temp;
-        temp = array.substr(array.indexOf(index) + index.length, array.indexOf(close) + close.length);
-        console.log(temp);
-        while (pos != -1) {
-
-            array = array.replace(temp, '');
-            pos = array.indexOf(index, pos);
-        }
-        return array;
-    }*/
-    //console.log(json);
-
 });
